dcac4ccbb6ff8367ff0c0a030c9866eee4ec2879
minhtri06ltr <laptopdienthoai1@gmail.com>
2021-12-19 10:00:33 +0000

    center hover

--- client/src/components/item/ListItem.jsx
+++ client/src/components/item/ListItem.jsx
@@ -4,11 +4,19 @@ import {
   ThumbDownAltOutlined,
   ThumbUpAltOutlined,
 } from "@material-ui/icons";
+import { useState } from "react";
 import "./listitem.scss";
 
-const ListItem = () => {
+const ListItem = ({ index }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
-    <div className="listItem">
+    <div
+      className="listItem"
+      style={{ left: isHovered && index * 255 - 50 + index * 2.5 }}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
       <img
         src="https://i.pinimg.com/originals/9c/74/52/9c7452da204cea4cb7a0f9a8ce12f126.jpg"
         alt=""
@@ -20,6 +28,17 @@ const ListItem = () => {
           <ThumbUpAltOutlined />
           <ThumbDownAltOutlined />
         </div>
+        <div className="itemInfoTop">
+          <span>1 hour 14 mins</span>
+          <span className="limit">+16</span>
+          <span>1999</span>
+        </div>
+        <div className="desc">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel
+          consequatur error cumque nihil natus necessitatibus ullam, dolorem
+          autem repellendus. Officia.
+        </div>
+        <div className="genra">Action</div>
       </div>
     </div>
   );
