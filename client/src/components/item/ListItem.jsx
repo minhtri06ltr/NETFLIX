fcd41542c07ef941348b587f9563a92170dd9d0b
minhtri06ltr <laptopdienthoai1@gmail.com>
2021-12-19 11:20:09 +0000

    finish homepage

--- client/src/components/item/ListItem.jsx
+++ client/src/components/item/ListItem.jsx
@@ -9,10 +9,12 @@ import "./listitem.scss";
 
 const ListItem = ({ index }) => {
   const [isHovered, setIsHovered] = useState(false);
-
+  const trailer =
+    "https://player.vimeo.com/external/371433846.sd.mp4?s=236da2f3c0fd273d2c6d9a064f3ae35579b2bbdf&profile_id=139&oauth2_token_id=57447761";
   return (
     <div
       className="listItem"
+      //absolute with list and move to away from left = item width to center preview
       style={{ left: isHovered && index * 255 - 50 + index * 2.5 }}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
@@ -21,25 +23,31 @@ const ListItem = ({ index }) => {
         src="https://i.pinimg.com/originals/9c/74/52/9c7452da204cea4cb7a0f9a8ce12f126.jpg"
         alt=""
       />
-      <div className="itemInfo">
-        <div className="icons">
-          <PlayArrow />
-          <Add />
-          <ThumbUpAltOutlined />
-          <ThumbDownAltOutlined />
-        </div>
-        <div className="itemInfoTop">
-          <span>1 hour 14 mins</span>
-          <span className="limit">+16</span>
-          <span>1999</span>
-        </div>
-        <div className="desc">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel
-          consequatur error cumque nihil natus necessitatibus ullam, dolorem
-          autem repellendus. Officia.
-        </div>
-        <div className="genra">Action</div>
-      </div>
+      {isHovered && (
+        <>
+          <video src={trailer} autoPlay={true} loop />
+          <div className="itemInfo">
+            <div className="icons">
+              <PlayArrow className="icon" />
+              <Add className="icon" />
+              <ThumbUpAltOutlined className="icon" />
+              <ThumbDownAltOutlined className="icon" />
+            </div>
+            <div className="itemInfoTop">
+              <span>1 hour 14 mins</span>
+              <span className="limit">+16</span>
+              <span>1999</span>
+            </div>
+            <div className="desc">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel
+              consequatur error cumque nihil natus necessitatibus ullam, dolorem
+              autem repellendus. Officia. Lorem ipsum dolor sit amet
+              consectetur, adipisicing elit. Aspernatur, reiciendis.
+            </div>
+            <div className="genra">Action</div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
