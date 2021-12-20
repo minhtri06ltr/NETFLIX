be355e5f389cee6eff7c02693ac632b815d3ea5a
minhtri06ltr <laptopdienthoai1@gmail.com>
2021-12-20 06:34:06 +0000

    login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
 import "./app.scss";
 
 import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import Watch from "./pages/watch/Watch";
 
 const App = () => {
   return (
     <>
-      <Register />
+      <Login />
     </>
   );
 };
