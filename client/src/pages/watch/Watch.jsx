4233efa3a32f216b1cf76b931d2ae0c6276e18f8
minhtri06ltr <laptopdienthoai1@gmail.com>
2021-12-20 06:09:31 +0000

    register page

--- client/src/pages/watch/Watch.jsx
+++ client/src/pages/watch/Watch.jsx
@@ -1,14 +1,20 @@
 import "./watch.scss";
 import { ArrowBackOutlined } from "@material-ui/icons";
-import mp4 from "../../assets/mp4/mumei.mp4";
+import mp4 from "../../assets/mp4/ina.mp4";
 
 const Watch = () => {
   return (
     <div className="watch">
       <div className="back">
-        <ArrowBackOutlined />
+        <ArrowBackOutlined /> Home
       </div>
-      <video src={mp4} autoPlay progress controls className="video"></video>
+      <video
+        src={mp4}
+        autoPlay
+        progress="true"
+        controls
+        className="video"
+      ></video>
     </div>
   );
 };
