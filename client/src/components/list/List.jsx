f60abb14132c68739021eb870eb88dc6e3fee8ae
laptopdienthoaimaytinhbang <laptopdienthoaimaytinhbang@gmail.com>
2022-01-08 06:55:20 -0500

    slider bug

--- client/src/components/list/List.jsx
+++ client/src/components/list/List.jsx
@@ -8,6 +8,7 @@ import "./list.scss";
 
 const List = ({ list }) => {
   const [slideNumber, setSlideNumber] = useState(0);
+  const [clickLimit, setClickLimit] = useState(window.innerWidth / 260);
   const [isMove, setIsMove] = useState(false);
   //get element by hook
   const listRef = useRef();
@@ -20,7 +21,7 @@ const List = ({ list }) => {
       setSlideNumber(slideNumber - 1);
       listRef.current.style.transform = `translateX(${260 + distance}px)`;
     }
-    if (direction === "right" && slideNumber <= 5) {
+    if (direction === "right" && slideNumber < 10 - clickLimit) {
       setSlideNumber(slideNumber + 1);
       listRef.current.style.transform = `translateX(${-260 + distance}px)`;
     }
