b09fd6720dddc359302ba3472656be45d4a60e81
laptopdienthoaimaytinhbang <laptopdienthoaimaytinhbang@gmail.com>
2022-01-09 06:55:05 -0500

    config heroku server

--- server/server.js
+++ server/server.js
@@ -32,6 +32,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/movies", movieRoutes);
 app.use("/api/lists", listRoutes);
-app.listen(5000, () => {
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
   console.log("Server is running ");
 });
