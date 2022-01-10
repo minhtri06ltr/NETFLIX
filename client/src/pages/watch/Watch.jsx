d75e995613c08201667694db4e68c5edfc197538
minhtri06ltr <79675694+minhtri06ltr@users.noreply.github.com>
2022-01-10 09:59:35 +0700

    Update Watch.jsx

--- client/src/pages/watch/Watch.jsx
+++ client/src/pages/watch/Watch.jsx
@@ -14,7 +14,7 @@ const Watch = () => {
       </Link>
       <iframe
         className="video"
-        src="https://www.youtube.com/embed/sYF3SiX5-WY"
+        src={location.state.movie.video}
         title={location.state.movie._id}
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
